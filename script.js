29fd38aac4bb2a4ad3747d72fc3043a336164c8d
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-07-18 05:47:08 +0300

    made changes to script.jas

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+  function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText === "") {
+      alert("Kindly enter a task");
+    } else {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = "taskText";
+      const removeButton = document.createElement("button");
+      removeButton.textContent = "Remove";
+      removeButton.className = "remove-btn";
+      removeButton.onclick = function () {
+        taskList.removeChild(taskItem);
+      };
+      // Append the remove button to the task item
+      taskItem.appendChild(removeButton);
+
+      // Append the task item to the task list
+      taskList.appendChild(taskItem);
+
+      // Clear the input field
+      taskInput.value = "";
+    }
+  }
+  addButton.addEventListener("click",addTask)
+  taskInput.addEventListener("keypress", function(event){
+    if (event.key === 'Enter') {
+     addTask();
+                }
+              document.addEventListener("DOMContentLoaded", function () {
+            addTask();  
+
+  });
+
+  
+});
